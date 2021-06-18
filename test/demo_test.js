e95646f72d36782fde570190951f490ac9862bb1
SimonGPort <simon.giroux.portelance@gmail.com>
2021-06-18 12:28:26 -0400

    more mocha test

--- test/demo_test.js
+++ test/demo_test.js
@@ -1,6 +1,8 @@
-const mocha=require('mocha');
+// const mocha=require('mocha');
 const assert=require('assert');
 
+//pour lancer un test mocha, il faut faire npm run test_mocha, voir dans mon package.json => ensuite ca va executer les tests dans le dossier test
+//on a meme pas besoin d'importer mocha
 describe('some demo tests',function(){
 
     //create tests
@@ -10,8 +12,11 @@ let aswers=[4,10,16]
 
 let doubleOperation=(elem)=>2*elem
 
+//assert va returner true ou false, pass or fail
 questions.forEach((elem,idx)=>{assert(doubleOperation(elem)===aswers[idx])})
-
     })
+
+    it('new test: addition 2+3',function(){
+        assert(2+3===5)})
 
 })
---
+++ test/demo_test2.js
@@ -0,0 +1,12 @@
+// const mocha=require('mocha');
+const assert=require('assert');
+
+//pour lancer un test mocha, il faut faire npm run test_mocha, voir dans mon package.json => ensuite ca va executer les tests dans le dossier test
+//on a meme pas besoin d'importer mocha
+describe('some demo tests',function(){
+
+    //create tests
+    it('test addition 2+3',function(){
+assert(2+3===5)})
+
+})
