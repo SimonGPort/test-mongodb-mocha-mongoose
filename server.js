af41fc13e40c1db5220445675dd88709af34467c
SimonGPort <simon.giroux.portelance@gmail.com>
2021-06-17 20:30:20 -0400

    initialize server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+let express = require('express')
+let app = express()
+let route = express.Router()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(4000, '0.0.0.0', () => { console.log("Server running on port 4000") })
